5761acddb17e84dc46dfcb8d9e775f534b53caca
Elie Andraos <elieandraos31@gmail.com>
2021-07-27 23:13:28 +0300

    :art: improve code format

--- tests/different.test.js
+++ tests/different.test.js
@@ -1,19 +1,19 @@
 import { different } from "./../src/index";
 
 test('passes with valid conditions', () => {
-    expect(different('foo', 'bar')).toBe(true);
-    expect(different(10, 20)).toBe(true);
-    expect(different(1, 'foo')).toBe(true);
-    expect(different(1, '1')).toBe(true);
-    expect(different(['foo', 'bar'], ['bar', 'foo'])).toBe(true);
-    expect(different({foo: 'bar'}, {bar: 'bar'})).toBe(true);
+    expect( different('foo', 'bar')).toBe(true);
+    expect( different(10, 20)).toBe(true);
+    expect( different(1, 'foo')).toBe(true);
+    expect( different(1, '1')).toBe(true);
+    expect( different(['foo', 'bar'], ['bar', 'foo'])).toBe(true);
+    expect( different({foo: 'bar'}, {bar: 'bar'})).toBe(true);
     expect( different(1, undefined) ).toBe(true);
     expect( different(1, null ) ).toBe(true);
 });
 
 test('fails with invalid conditions', () => {
-    expect(different('foo', 'foo')).toBe(false);
-    expect(different(1, 1)).toBe(false);
-    expect(different(['foo', 'bar'], ['foo', 'bar'])).toBe(false);
-    expect(different({foo: 'bar'}, {foo: 'bar'})).toBe(false);
+    expect( different('foo', 'foo') ).toBe(false);
+    expect( different(1, 1) ).toBe(false);
+    expect( different(['foo', 'bar'], ['foo', 'bar']) ).toBe(false);
+    expect( different({foo: 'bar'}, {foo: 'bar'}) ).toBe(false);
 });

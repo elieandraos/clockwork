b1f8709a32787bbd324987464701343a49a9d80c
Elie Andraos <elieandraos31@gmail.com>
2021-07-27 23:30:16 +0300

    :sparkles: add matches_regex rule

--- src/index.js
+++ src/index.js
@@ -106,6 +106,10 @@ const uuid = function(value) {
     return new RegExp("^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$").test(String(value).toLowerCase());
 }
 
+export function matches_regex(value, arg = null) {
+    return new RegExp(arg).test(String(value).toLowerCase());
+}
+
 module.exports = {
     required,
     string,
@@ -127,5 +131,6 @@ module.exports = {
     different,
     url,
     email,
-    uuid
+    uuid,
+    matches_regex
 }

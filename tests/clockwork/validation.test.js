71ba73c0079dcb1b2b7185957fbed1da6aa96f76
Elie Andraos <elieandraos31@gmail.com>
2021-08-13 20:33:10 +0300

    :white_check_mark: add 'sometimes' rule tests

--- tests/clockwork/validation.test.js
+++ tests/clockwork/validation.test.js
@@ -9,7 +9,6 @@ test("it passes when all the rules pass", () => {
 
     expect(clockwork.passes()).toBe(true);
     expect(clockwork.fails()).toBe(false);
-    // test error bag is empty
 });
 
 test("it fails when at least one rule fails", () => {
@@ -18,6 +17,28 @@ test("it fails when at least one rule fails", () => {
     clockwork.setRules(rules).setData(data);
 
     expect(clockwork.passes()).toBe(false);
-    expect(clockwork.fails()).toBe(true);
-    // test error bag is not empty
+});
+
+test("'sometimes' rule prevents the other rules from executing if the data is not present", () => {
+    let rules = { name: 'sometimes | string | email'};
+    let data = { name: null };
+    clockwork.setRules(rules).setData(data);
+
+    expect(clockwork.passes()).toBe(true);
+});
+
+test("'sometimes' rule allows the other rules to execute if the data is present", () => {
+    let rules = { name: 'sometimes | string | email'};
+    let data = { name: 'foo' };
+    clockwork.setRules(rules).setData(data);
+
+    expect(clockwork.passes()).toBe(false);
+});
+
+test("'sometimes' order in the rule string does not matter", () => {
+    let rules = { name: 'string | email | sometimes'};
+    let data = { name: null };
+    clockwork.setRules(rules).setData(data);
+
+    expect(clockwork.passes()).toBe(true);
 });

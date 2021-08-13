1e7dae82613ef1c0de135e7d1aaf2d7ce0c75459
Elie Andraos <elieandraos31@gmail.com>
2021-08-13 21:29:30 +0300

    :sparkles: cover more test cases for matches_regex

--- tests/rules/matches_regex.test.js
+++ tests/rules/matches_regex.test.js
@@ -2,6 +2,8 @@ import { matches_regex } from "../../src/rules";
 
 test('passes with valid pattern', () => {
     expect( matches_regex('foobar', '^[A-Za-z]+$') ).toBe(true);
+    expect( matches_regex('foobar', null) ).toBe(false);
+    expect( matches_regex('foobar', undefined) ).toBe(false);
 });
 
 test('fails with invalid pattern', () => {

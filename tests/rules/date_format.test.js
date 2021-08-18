c51d25e463fa698c69e40fc8c7425086ac7f3cbc
Elie Andraos <elieandraos31@gmail.com>
2021-08-19 02:07:38 +0300

    :white_check_mark: add date_format rule tests

---
+++ tests/rules/date_format.test.js
@@ -0,0 +1,22 @@
+import { date_format } from "../../src/rules";
+
+test("passes with valid date format", () => {
+    expect( date_format('1979-10-22', 'YYYY-MM-DD') ).toBe(true);
+    expect( date_format('1979-10-22', 'YYYY/MM/DD') ).toBe(false);
+})
+
+test("fails with null", () => {
+    expect( date_format(null) ).toBe(false);
+})
+
+test("fails with string", () => {
+    expect( date_format('foo') ).toBe(false);
+})
+
+test("fails with array", () => {
+    expect( date_format([]) ).toBe(false);
+})
+
+test("fails with object", () => {
+    expect( date_format({}) ).toBe(false);
+})

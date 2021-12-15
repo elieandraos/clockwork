f52757c23789c61967be87abf8f00db241631356
Elie Andraos <elieandraos31@gmail.com>
2021-12-15 10:04:24 +0300

    :green_heart: CI scripts

---
+++ scripts/build.js
@@ -0,0 +1,23 @@
+const esbuild = require('esbuild')
+const { nodeExternalsPlugin } = require('esbuild-node-externals')
+
+esbuild
+    .build({
+        entryPoints: ['src/clockwork.js'],
+        bundle: true,
+        minify: true,
+        logLevel: 'info',
+        platform: 'node',
+        outfile: 'dist/index.js',
+        plugins: [
+            nodeExternalsPlugin({
+                dependencies: true,
+                devDependencies: false,
+                peerDependencies: false,
+                optionalDependencies: false,
+            }),
+        ],
+    })
+    .catch(() => {
+        process.exit(1)
+    })

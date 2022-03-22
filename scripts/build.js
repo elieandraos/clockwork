bb3c583db7c5e51ef97eab0bb4bac1dddaeb6a32
Elie Andraos <elieandraos31@gmail.com>
2022-03-22 17:45:06 +0200

    :wrench: exclude node external plugins in build file

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,5 @@
 const esbuild = require('esbuild')
+const { nodeExternalsPlugin } = require('esbuild-node-externals')
 
 // ESM module (ECMAScript)
 // Meant for "import" statements in (modern browsers and bundlers)
@@ -9,6 +10,14 @@ esbuild.build({
     minify: true,
     format: 'esm',
     target: ['esnext'],
+    plugins: [
+        nodeExternalsPlugin({
+            dependencies: true,
+            devDependencies: false,
+            peerDependencies: false,
+            optionalDependencies: false,
+        }),
+    ],
     logLevel: 'info',
 }).catch( () => {
     process.exit(1)
@@ -23,6 +32,14 @@ esbuild.build({
     minify: true,
     platform: 'node',
     target: ['node10.4'],
+    plugins: [
+        nodeExternalsPlugin({
+            dependencies: true,
+            devDependencies: false,
+            peerDependencies: false,
+            optionalDependencies: false,
+        }),
+    ],
     logLevel: 'info',
 }).catch( () => {
     process.exit(1)
